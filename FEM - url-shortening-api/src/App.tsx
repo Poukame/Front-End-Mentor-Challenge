1294fcd8ad96398902f3c0dfb97916f1abadaa70
Poukame <theretg@gmail.com>
2022-07-14 14:32:07 +0300

    desktop and mobile layout OK

--- FEM - url-shortening-api/src/App.tsx
+++ FEM - url-shortening-api/src/App.tsx
@@ -5,10 +5,18 @@ import Hero from './components/Hero';
 import Shortener from './components/Shortener';
 import Footer from './components/Footer';
 
+
+
 function App() {
+
+	const [display, setDisplay] = useState(false);
+
 	return (
 		<>
-			<Header />
+			<Header 
+			handleClick={() => setDisplay(!display)}
+			display={display}
+			/>
 			<main>
 				<Hero />
 				<Shortener />
--- FEM - url-shortening-api/src/components/Header.tsx
+++ FEM - url-shortening-api/src/components/Header.tsx
@@ -1,20 +1,54 @@
 import React from 'react';
 import logo from '../images/logo.svg';
 
-export default function Header() {
+export default function Header({display, handleClick}) {
 	return (
 		<header>
 			<img src={logo} className='logo header__logo' alt='logo of Shortly' />
-			<span className='hamburger'>|||</span>
-			<nav className='header__nav'>
-				<ul className='header__nav-ul'>
-					<li><a href="#">Features</a></li>
-					<li><a href="#">Pricing</a></li>
-					<li><a href="#">LResources</a></li>
-					<li><a href="#">Login</a></li>
-					<li><a href="#">Sign-up</a></li>
+			<span className='hamburger' onClick={handleClick}>
+				|||
+			</span>
+			<nav className='header__nav desktop--nav'>
+				<ul className='header__nav-ul desktop_nav--ul'>
+					<li>
+						<a href='#'>Features</a>
+					</li>
+					<li>
+						<a href='#'>Pricing</a>
+					</li>
+					<li>
+						<a href='#'>Resources</a>
+					</li>
+					<li>
+						<a href='#'>Login</a>
+					</li>
+					<li>
+						<a href='#'>Sign-up</a>
+					</li>
 				</ul>
 			</nav>
+			{display && (
+				<nav className='header__nav mobile--nav'>
+					<ul className='header__nav-ul mobile_nav--ul'>
+						<li>
+							<a href='#'>Features</a>
+						</li>
+						<li>
+							<a href='#'>Pricing</a>
+						</li>
+						<li>
+							<a href='#'>Resources</a>
+						</li>
+						<hr />
+						<li>
+							<a href='#'>Login</a>
+						</li>
+						<li>
+							<a href='#'>Sign-up</a>
+						</li>
+					</ul>
+				</nav>
+			)}
 		</header>
 	);
 }
