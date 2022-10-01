b7b0cb6cb32352de6ee56f58596e71dc85c34a02
Poukame <theretg@gmail.com>
2022-10-01 15:00:28 +0300

    bcColor fix

--- FEM - rock-paper-scissors/src/theme.ts
+++ FEM - rock-paper-scissors/src/theme.ts
@@ -56,6 +56,5 @@ const body = {
 
 // // 3. extend the theme
 const theme = extendTheme({ breakpoints }, fonts, body, colors);
-console.log('file: theme.ts ~ line 57 ~ theme', theme);
 
 export default theme;
